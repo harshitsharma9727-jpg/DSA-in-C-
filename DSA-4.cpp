074697ffca1282cd32ab7b00af1dc48efd602189
harshitsharma9727-jpg <harshitsharma9727@gmail.com>
2026-03-12 12:56:39 +0530

    DSA Day-4 check prime number

--- DSA-4.cpp
+++ DSA-4.cpp
@@ -1,11 +1,29 @@
-n = int(input("enter a number: "))
+#include<iostream>
+using namespace std;
 
-if n <= 1:
-    print("Not a Prime Number")
-else:
-    for i in range(2, n):
-        if n % i == 0:
-            print("Not a Prime Number")
-            break
-    else:
-        print("Prime Number")
+int main()
+{
+    int n,i,count=0;
+
+    cout<<"Enter number: ";
+    cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+    }
+
+    if(count==2)
+    {
+        cout<<"Prime number";
+    }
+    else
+    {
+        cout<<"Not prime number";
+    }
+
+    return 0;
+}
