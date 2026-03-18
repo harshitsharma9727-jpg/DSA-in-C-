17387f63eeb2cf4f3ec8b9fb0f8c7562fd3b4c18
harshitsharma9727-jpg <harshitsharma9727@gmail.com>
2026-03-18 12:49:17 +0530

    DSA Day9 print all the divisors of N in the ascending order.

---
+++ DSA DAY-9.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"enter number: ";
+    cin>>n;
+
+    cout<<"divisors are:\n";
+
+    for(int i=1;i<=n;i++)
+    {
+        if(n % i == 0)
+        {
+            cout<<i<<" ";
+        }
+    }
+
+    return 0;
+}
