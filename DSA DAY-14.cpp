697a2a6ba221e709a181a7e51f4036c9056bdf6b
harshitsharma9727-jpg <harshitsharma9727@gmail.com>
2026-03-22 12:40:39 +0530

    DSA DAY-14, This program takes an array input from the user and moves all zeros to the end while maintaining the order of

---
+++ DSA DAY-14.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter size: ";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"enter elments:\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    int j = 0;  
+
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i] != 0)
+        {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+    cout<<"array After moving zero :\n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
