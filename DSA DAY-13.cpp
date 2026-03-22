b9f8ab9e14b802e2c54758131bb087ee91f16418
harshitsharma9727-jpg <harshitsharma9727@gmail.com>
2026-03-22 12:33:51 +0530

    DSA Day-13, reverse the array

---
+++ DSA DAY-13.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter size: ";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"eter elements:\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    int start = 0, end = n-1;
+
+    while(start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    cout<<"reversed array:\n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
